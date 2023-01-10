eedfba67398f10c4816f4519b8c6996d76d63fcb
Ta9yaSa10 <takuya_sato@aidma-hd.jp>
2023-01-10 18:27:33 +0900

    split file

--- lambda_src/index.js
+++ lambda_src/index.js
@@ -1,53 +1,54 @@
-import fetch from 'node-fetch';
+import { getLineCurrentMsgCount, createMsgFromLineRes, postSlackWebhookApi } from './utils.js'
+// import fetch from 'node-fetch';
 import 'date-utils'
-const env = process.env;
-let distSlackMsg;
+// const env = process.env;
+// let distSlackMsg;
 
 export const handler = async (event) => {
 
     // LineAPI
-    const getLineCurrentMsgCount = async () => {
-        const date = new Date().toFormat('yyyyMMdd');
-        const params = `?date=${date}`;
-        try{
-            const resFromLineResponse = await fetch(`${env.lineApiUrl}${params}`, {
-                    headers: { 'Authorization': `Bearer ${env.lineApiToken}` }
-                  });
-            const { totalUsage } = await resFromLineResponse.json();
-            return totalUsage;
-        } catch(err){
-            throw new err("LINEリクエスト失敗")
-        }
-    }
+    // const getLineCurrentMsgCount = async () => {
+    //     const date = new Date().toFormat('yyyyMMdd');
+    //     const params = `?date=${date}`;
+    //     try{
+    //         const resFromLineResponse = await fetch(`${env.lineApiUrl}${params}`, {
+    //                 headers: { 'Authorization': `Bearer ${env.lineApiToken}` }
+    //               });
+    //         const { totalUsage } = await resFromLineResponse.json();
+    //         return totalUsage;
+    //     } catch(err){
+    //         throw new err("LINEリクエスト失敗")
+    //     }
+    // }
 
     // Lineメッセージ残数とメッセージ対応付
-    const createMsgFromLineRes = async (resData) => {
-        if(env.threshold < resData || env.threshold == resData) {
-            distSlackMsg = `当月のpushメッセージ数が${env.threshold}以上! :alert: \n 追加申請をお願いします!当月pushメッセージ数:${resData}`;
-        } else if(env.threshold > resData) {
-            distSlackMsg = `当月のpushメッセージ数は${env.threshold}以内です。:sunny: \n 当月pushメッセージ数:${resData}`;
-        }
-        return distSlackMsg;
-    }
+    // const createMsgFromLineRes = async (resData) => {
+    //     if(env.threshold < resData || env.threshold == resData) {
+    //         distSlackMsg = `当月のpushメッセージ数が${env.threshold}以上! :alert: \n 追加申請をお願いします!当月pushメッセージ数:${resData}`;
+    //     } else if(env.threshold > resData) {
+    //         distSlackMsg = `当月のpushメッセージ数は${env.threshold}以内です。:sunny: \n 当月pushメッセージ数:${resData}`;
+    //     }
+    //     return distSlackMsg;
+    // }
     
     // Slackのwebhook実行
-    const postSlackWebhookApi = async (distSlackMsg) => {
-        let headers = {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${env.slackBotToken}`
-        }
-        let body = {
-            channel: env.slackChannelId,
-            text: distSlackMsg
-        }
-        try{
-            const webHookUrl = env.webHookUrl;
-            const res = await fetch(webHookUrl, {method: 'POST', body: JSON.stringify(body), headers: headers});
-            if(!res.ok) throw new Error("slackチャネルへのメッセージ送信に失敗しました");
-        } catch(e) {
-            error.log(e);
-        }
-    }
+    // const postSlackWebhookApi = async (distSlackMsg) => {
+    //     let headers = {
+    //         "Content-Type": "application/json",
+    //         "Authorization": `Bearer ${env.slackBotToken}`
+    //     }
+    //     let body = {
+    //         channel: env.slackChannelId,
+    //         text: distSlackMsg
+    //     }
+    //     try{
+    //         const webHookUrl = env.webHookUrl;
+    //         const res = await fetch(webHookUrl, {method: 'POST', body: JSON.stringify(body), headers: headers});
+    //         if(!res.ok) throw new Error("slackチャネルへのメッセージ送信に失敗しました");
+    //     } catch(e) {
+    //         error.log(e);
+    //     }
+    // }
 
     try {
         // LineAPI
---
+++ lambda_src/utils.js
@@ -0,0 +1,47 @@
+import fetch from 'node-fetch';
+import 'date-utils'
+let distSlackMsg;
+const env = process.env;
+// LineAPI
+export const getLineCurrentMsgCount = async () => {
+    const date = new Date().toFormat('yyyyMMdd');
+    const params = `?date=${date}`;
+    try{
+        const resFromLineResponse = await fetch(`${env.lineApiUrl}${params}`, {
+                headers: { 'Authorization': `Bearer ${env.lineApiToken}` }
+              });
+        const { totalUsage } = await resFromLineResponse.json();
+        return totalUsage;
+    } catch(err){
+        console.error(err)
+    }
+}
+
+// Lineメッセージ残数とメッセージ対応付
+export const createMsgFromLineRes = async (resData) => {
+    if(env.threshold < resData || env.threshold == resData) {
+        distSlackMsg = `当月のpushメッセージ数が${env.threshold}以上! :alert: \n 追加申請をお願いします!当月pushメッセージ数:${resData}`;
+    } else if(env.threshold > resData) {
+        distSlackMsg = `当月のpushメッセージ数は${env.threshold}以内です。:sunny: \n 当月pushメッセージ数:${resData}`;
+    }
+    return distSlackMsg;
+}
+
+// Slackのwebhook実行
+export const postSlackWebhookApi = async (distSlackMsg) => {
+    let headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${env.slackBotToken}`
+    }
+    let body = {
+        channel: env.slackChannelId,
+        text: distSlackMsg
+    }
+    try{
+        const webHookUrl = env.webHookUrl;
+        const res = await fetch(webHookUrl, {method: 'POST', body: JSON.stringify(body), headers: headers});
+        if(!res.ok) throw new Error("slackチャネルへのメッセージ送信に失敗しました");
+    } catch(err) {
+        console.error(err);
+    }
+}
